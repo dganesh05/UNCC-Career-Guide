930611b0c41dfa67067b3ecb7bba1b75524e79eb
[jquinn24] <[jquinn24@charlotte.edu]>
2025-04-22 17:07:46 -0400

    url fixes for resources

--- uncc_career_guide/urls.py
+++ uncc_career_guide/urls.py
@@ -58,6 +58,7 @@ urlpatterns = [
     path('career-advisor/', include('career_advisor.urls')),
     path('chat/', chat_view, name='chat'),
     path('api/chat/', views.ChatbotView.as_view(), name='chat_api'),
+    path('generate-resume/', resume_views.generate_resume, name='generate_resume'),
 ]
 
 if settings.DEBUG:
